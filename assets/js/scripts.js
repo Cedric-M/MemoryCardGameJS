0518628316c0b3a0246cb1462fb8156c28f205cc
Cédric Montes <cirdec95@gmail.com>
2018-08-18 00:57:53 +0200

    Update: scripts.js Clicked Cards are now strored into variables

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,24 @@
 const cards = document.querySelectorAll('.memory-card'); //List of all memory-card element
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
 function flipCard() {
 	this.classList.toggle('flip')
+
+	if (!hasFlippedCard) { //This is the first time the player has clicked a card
+		hasFlippedCard = true;
+		firstCard = this;
+
+	} else {
+		//Second click
+		hasFlippedCard = false; 
+		secondCard = this;
+
+		// Does the 2 cards match ?
+		console.log(firstCard.dataset.name);
+		console.log(secondCard.dataset.name);
+	}
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
